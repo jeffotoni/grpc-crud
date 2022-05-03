4374876cc6edb0ee936d743dc6a81195006f257a
jeffeotoni <jeff.otoni@gmail.com>
2022-05-03 11:40:20 -0300

    organization

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"context"
-	"flag"
+	"fmt"
 	"log"
+//	"strconv"
 	"time"
 
 	pb "github.com/jeffotoni/grpc-crud/proto"
@@ -14,8 +15,8 @@ import (
 var Addr = "localhost:4001"
 
 func main() {
-	id := flag.String("id", "", "document")
-	flag.Parse()
+	// id := flag.String("id", "", "document")
+	// flag.Parse()
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
@@ -26,14 +27,30 @@ func main() {
 	defer conn.Close()
 	client := pb.NewCustomersServiceClient(conn)
 
-	log.Println("Get:")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	println("here:", *id)
-	getReq := &pb.GetRequest{Document: *id}
-	customer, err := client.Get(ctx, getReq)
-	if err != nil {
-		log.Fatalf("%v.Get(_) = _, %v: ", client, err)
-	}
-	log.Println(customer)
+	ticker := time.NewTicker(time.Millisecond * time.Duration(50))
+	done := make(chan bool)
+	i := 0
+	go func(i int) {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				i++
+				fmt.Println("ticker:", t)
+				//istr := strconv.Itoa(i)
+				getReq := &pb.GetRequest{Document: "43009665923"}
+				customer, err := client.Get(context.Background(), getReq)
+				if err != nil {
+					log.Fatalf("%v.Get(_) = _, %v: ", client, err)
+				}
+				log.Println(customer)
+			}
+		}
+	}(i)
+
+	time.Sleep(time.Second * time.Duration(60))
+	ticker.Stop()
+	done <- true
+	println("done")
 }
